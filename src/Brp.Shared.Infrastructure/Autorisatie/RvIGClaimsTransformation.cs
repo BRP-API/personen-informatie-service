d4249c99b4851cde18ec792649b6aa73a4a4d020
MelvLee <github-action@actions-template-sync.noreply.github.com>
2024-08-12 15:29:57 +0000

    chore: sync met 'BRP-API/brp-shared-dotnet' repo
    Signed-off-by: MelvLee <github-action@actions-template-sync.noreply.github.com>

---
+++ src/Brp.Shared.Infrastructure/Autorisatie/RvIGClaimsTransformation.cs
@@ -0,0 +1,39 @@
+﻿using Brp.Shared.Infrastructure.Json;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json.Linq;
+using System.Security.Claims;
+
+namespace Brp.Shared.Infrastructure.Autorisatie;
+
+public class RvIGClaimsTransformation : IClaimsTransformation
+{
+    private readonly IHttpContextAccessor _httpContextAccessor;
+
+    public RvIGClaimsTransformation(IHttpContextAccessor httpContextAccessor)
+    {
+        _httpContextAccessor = httpContextAccessor;
+    }
+
+    public Task<ClaimsPrincipal> TransformAsync(ClaimsPrincipal principal)
+    {
+        List<Claim> claims = new();
+        JObject jObject = new();
+
+        var claimKeyValuePairs = from claim in principal.Claims
+                                 where claim.Type == "claims"
+                                 let values = claim.Value.Split('=')
+                                 select values;
+        foreach (var claimKeyValue in claimKeyValuePairs )
+        {
+            jObject.Add(claimKeyValue[0], claimKeyValue[1]);
+            claims.Add(new Claim(claimKeyValue[0], claimKeyValue[1]));
+        }
+
+        _httpContextAccessor.HttpContext?.Items.Add("Claims", jObject.ToJsonCompact());
+
+        principal.AddIdentity(new ClaimsIdentity(claims));
+
+        return Task.FromResult(principal);
+    }
+}
