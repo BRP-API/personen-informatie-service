7bd49055bcfdca447d3b0f10ce7b274240c25391
Melvin Lee <melvin.lee@iswish.nl>
2024-08-13 18:00:54 +0200

    ci: correcties in docker manifest en report generator script

--- scripts/generate-cucumber-reports.js
+++ scripts/generate-cucumber-reports.js
@@ -1,8 +1,25 @@
 const report = require('multiple-cucumber-html-reporter');
 
+const customData = {
+    title: 'BRP API',
+    data: [
+        { label: 'Applicatie', value: 'Personen Informatie Service' },
+        { label: 'Versie', value: `${process.argv[2]}` },
+        { label: 'Build', value: `${process.argv[3]}`},
+        { label: 'Branch', value: `${process.argv[4]}`}
+    ]
+};
 report.generate({
     jsonDir: 'test-reports/cucumber-js/step-definitions',
     reportPath: 'test-reports/cucumber-js/reports/step-definitions',
     reportName: 'Stap definities tests',
     hideMetadata: true
+});
+
+report.generate({
+    jsonDir: 'test-reports/cucumber-js/personen',
+    reportPath: 'test-reports/cucumber-js/reports/personen/informatie-service',
+    reportName: 'Personen informatie service features',
+    hideMetadata: true,
+    customData: customData
 });
