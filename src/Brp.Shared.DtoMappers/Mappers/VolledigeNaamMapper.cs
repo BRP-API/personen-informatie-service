0998428f9197ef2df7e035e062bd95ed47c2b1b4
nkorporaal <nap.korporaal+github@gmail.com>
2024-12-13 11:37:36 +0100

    fix: sync VolledigeNaamMapper met Brp.Shared.DtoMappers

--- src/Brp.Shared.DtoMappers/Mappers/VolledigeNaamMapper.cs
+++ src/Brp.Shared.DtoMappers/Mappers/VolledigeNaamMapper.cs
@@ -9,7 +9,7 @@ public static class VolledigeNaamMapper
         var adellijkeTitel = naam.AdellijkeTitelPredicaat.MapNaarAdellijkeTitel(geslacht);
         var predikaat = naam.AdellijkeTitelPredicaat.MapNaarPredicaat(geslacht);
 
-        var retval = $"{predikaat} {naam.Voornamen} {adellijkeTitel} {naam.Achternaam()}".RemoveRedundantSpaces();
+        var retval = $"{predikaat} {naam.Voornamen} {adellijkeTitel} {naam.Voorvoegsel} {naam.Geslachtsnaam}".RemoveRedundantSpaces();
 
         return !string.IsNullOrWhiteSpace(retval)
             ? retval
