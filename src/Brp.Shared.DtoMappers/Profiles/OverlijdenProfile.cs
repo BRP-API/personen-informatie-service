10168803450d8e56c8e549689ed2559d024906bd
Melvin Lee <melvin.lee@iswish.nl>
2026-09-16 07:55:36 +0200

    chore(cleanup): manually delete the profile classes
    repo-sync doesn't sync file deletion

--- src/Brp.Shared.DtoMappers/Profiles/OverlijdenProfile.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Brp.Shared.DtoMappers.Profiles;
-
-public class OverlijdenProfile : DatumPlaatsLandDtoProfile<BrpDtos.GbaOverlijden, BrpApiDtos.Overlijden>
-{
-    public OverlijdenProfile()
-    {
-        CreateMap<BrpDtos.InOnderzoek, BrpApiDtos.OverlijdenInOnderzoek?>().ConvertUsing<OverlijdenInOnderzoekConverter>();
-    }
-}
